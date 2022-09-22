f098a7c71486cdae7349fe23403984d2d563738e
Sandro Smarzaro <sansmarzaro@gmail.com>
2022-09-21 22:36:04 -0300

    [ADDED] initial project

---
+++ src/main/java/one/digitalinnovation/shorttermparking/ShortTermParkingApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.shorttermparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortTermParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortTermParkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/one/digitalinnovation/shorttermparking/ShortTermParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.shorttermparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortTermParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
